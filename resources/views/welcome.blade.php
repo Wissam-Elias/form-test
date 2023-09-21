82c33d00fcc5c15a71d3af11ea33eec0a65c2df5
Wissam Elias <wissam.elias.111@gmail.com>
2023-09-21 12:43:59 +0300

    feat (resources): change the welcome blade and add bootstrap cdns

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 text-center">
-            <h1 class="display-4 mt-5">Welcome Sequenced Form Test Site</h1>
-            <p class="lead">This site is done by Wissam Elias!</p>
-            <a href="{{ route('login') }}" class="btn btn-primary btn-lg mt-3">Login</a>
-            <a href="{{ route('login') }}" class="btn btn-primary btn-lg mt-3">Register</a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <h1 class="display-4 mt-5">Welcome Sequenced Form Test Site</h1>
+                <p class="lead">This site is done by Wissam Elias!</p>
+                @guest
+                    <a href="{{ route('login') }}" class="btn btn-primary btn-lg mt-3">Login</a>
+                    <a href="{{ route('register') }}" class="btn btn-primary btn-lg mt-3">Register</a>
+                @else
+                    <h3>hello {{ Auth::user()->name }}</h3>
+                    <a href="{{ route('pg1') }}" class="btn btn-primary btn-lg mt-3">Get Started</a>
+                @endguest
+
+            </div>
         </div>
     </div>
-</div>
 @endsection
